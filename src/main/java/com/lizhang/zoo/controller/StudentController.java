5256415038467d044cf313e6ff8260e1f2be3525
chen.meng <649701771@qq.com>
2019-11-27 17:24:46 +0800

    [cm][level 0] url

--- src/main/java/com/lizhang/zoo/controller/StudentController.java
+++ src/main/java/com/lizhang/zoo/controller/StudentController.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/students")
+@RequestMapping("/getStudentScoreInfo")
 public class StudentController {
 
     @GetMapping
